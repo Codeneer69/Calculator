0d87fa93c4fba2d586b532f3cb7cee7fc40d05a8
Scrambled-Egg <70576373+Scrambled-Egg@users.noreply.github.com>
2020-09-01 16:05:40 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,284 @@
+
+#greeting stuff
+
+print ('\t    Welcome to the Python-made Calculator!! \n')
+
+print('\n')
+
+# user input variable
+
+x = str(input('Which operation do you want to do? - '))
+
+a = int(input('\nEnter the first number :- \n'))
+
+b = int(input('\nEnter the second number :- \n'))
+
+#IF ELIF WORKS
+
+# addition
+if x == "addition":
+	print ('The sum of these two numbers is',(a+b))
+
+elif x == "Addition":
+	print ('The sum of these two numbers is',(a+b))
+
+elif x == "ADDITION":
+	print ('The sum of these two numbers is',(a+b))
+
+
+#difference
+elif x == "difference":
+	if a>b:
+		print ('The difference between these two numbers is',(a-b))
+	else :
+		print ('The difference between these two numbers is',(b-a))
+		
+elif x == "Difference":
+	if a>b:
+		print ('The difference between these two numbers is',(a-b))
+	else :
+		print ('The difference between these two numbers is',(b-a))
+	
+elif x == "DIFFERENCE":
+	if a>b:
+		print ('The difference between these two numbers is',(a-b))
+	else :
+		print ('The difference between these two numbers is',(b-a))
+
+
+#multiplication
+
+elif x == "multiply":
+	print ('Product of these two numbers is',(a*b))
+
+elif x == "Multiply":
+	print ('Product of these two numbers is',(a*b))
+	
+elif x == "multiplication":
+	print ('Product of these two numbers is',(a*b))
+		
+elif x == "Multiplication":
+	print ('Product of these two numbers is',(a*b))	
+
+elif x == "product":
+	print ('Product of these two number is',(a*b))	
+
+elif x == "Product":
+	print ('Product of these two number is',(a*b))	
+
+elif x == "MULTIPLY":
+	print ('Product of these two numbers is',(a*b))
+	
+elif x == "MULTIPLICATION":
+	print ('Product of these two numbers is',(a*b))	
+	
+elif x == "PRODUCT":
+	print ('Product of these two numbers is',(a*b))
+	
+
+#substraction
+
+elif x == "substract":
+	print ('Second number will be substracted from the first.\n')
+	print ('The answer is',(a-b))
+	
+elif x == "Substract":
+	print ('Second number will be substracted from the first.\n')
+	print ('The answer is',(a-b))
+	
+elif x == "SUBSTRACT":
+	print ('Second number will be substracted from the first.\n')
+	print ('The answer is',(a-b))
+	
+elif x == "substraction":
+	print ('Second number will be substracted from the first.\n')
+	print ('The answer is',(a-b))
+	
+elif x == "Substraction":
+	print ('Second number will be substracted from the first.\n')
+	print ('The answer is',(a-b))
+	
+elif x == "SUBSTRACTION":
+	print ('Second number will be substracted from the first.\n')
+	print ('The answer is',(a-b))
+
+
+#division
+
+elif x == "division":
+	print ('\nChoose any one option among the following :- \n')
+
+	print ('ONE. I want to divide first number by the second number. \n')
+
+	print ('TWO. I want to divide second number by the first number. \n')
+	
+	d = str(input('Enter your chosen option here :-\n'))
+	
+	if d == 'one':
+		print ('\nThe answer is',(a/b))
+	
+	elif d == 'One':
+		print ('\nThe answer is',(a/b))
+		
+	elif d == 'ONE':
+		print ('\nThe answer is',(a/b))
+		
+	elif d == 'two':
+		print ('\nThe answer is',(b/a))
+	
+	elif d == 'Two':
+		print ('\nThe answer is',(b/a))
+		
+	elif d == 'TWO':
+		print ('\nThe answer is',(b/a))
+		
+elif x == "Division":
+	print ('\nChoose any one option among the following :- \n')
+
+	print ('ONE. I want to divide first number by the second number. \n')
+
+	print ('TWO. I want to divide second number by the first number. \n')
+	
+	d = str(input('Enter your chosen option here :-\n'))
+	
+	if d == 'one':
+		print ('\nThe answer is',(a/b))
+	
+	elif d == 'One':
+		print ('\nThe answer is',(a/b))
+		
+	elif d == 'ONE':
+		print ('\nThe answer is',(a/b))
+		
+	elif d == 'two':
+		print ('\nThe answer is',(b/a))
+	
+	elif d == 'Two':
+		print ('\nThe answer is',(b/a))
+		
+	elif d == 'TWO':
+		print ('\nThe answer is',(b/a))
+		
+elif x == "DIVISION":
+	print ('\nChoose any one option among the following :- \n')
+
+	print ('ONE. I want to divide first number by the second number. \n')
+
+	print ('TWO. I want to divide second number by the first number. \n')
+	
+	d = str(input('Enter your chosen option here :-\n'))
+	
+	if d == 'one':
+		print ('\nThe answer is',(a/b))
+	
+	elif d == 'One':
+		print ('\nThe answer is',(a/b))
+		
+	elif d == 'ONE':
+		print ('\nThe answer is',(a/b))
+		
+	elif d == 'two':
+		print ('\nThe answer is',(b/a))
+	
+	elif d == 'Two':
+		print ('\nThe answer is',(b/a))
+		
+	elif d == 'TWO':
+		print ('\nThe answer is',(b/a))
+		
+elif x == "divide":
+	print ('\nChoose any one option among the following :- \n')
+
+	print ('ONE. I want to divide first number by the second number. \n')
+
+	print ('TWO. I want to divide second number by the first number. \n')
+	
+	d = str(input('Enter your chosen option here :-\n'))
+	
+	if d == 'one':
+		print ('\nThe answer is',(a/b))
+	
+	elif d == 'One':
+		print ('\nThe answer is',(a/b))
+		
+	elif d == 'ONE':
+		print ('\nThe answer is',(a/b))
+		
+	elif d == 'two':
+		print ('\nThe answer is',(b/a))
+	
+	elif d == 'Two':
+		print ('\nThe answer is',(b/a))
+		
+	elif d == 'TWO':
+		print ('\nThe answer is',(b/a))
+		
+elif x == "Divide":
+	print ('\nChoose any one option among the following :- \n')
+
+	print ('ONE. I want to divide first number by the second number. \n')
+
+	print ('TWO. I want to divide second number by the first number. \n')
+	
+	d = str(input('Enter your chosen option here :-\n'))
+	
+	if d == 'one':
+		print ('\nThe answer is',(a/b))
+	
+	elif d == 'One':
+		print ('\nThe answer is',(a/b))
+		
+	elif d == 'ONE':
+		print ('\nThe answer is',(a/b))
+		
+	elif d == 'two':
+		print ('\nThe answer is',(b/a))
+	
+	elif d == 'Two':
+		print ('\nThe answer is',(b/a))
+		
+	elif d == 'TWO':
+		print ('\nThe answer is',(b/a))
+		
+elif x == "DIVIDE":
+	print ('\nChoose any one option among the following :- \n')
+
+	print ('ONE. I want to divide first number by the second number. \n')
+
+	print ('TWO. I want to divide second number by the first number. \n')
+	
+	d = str(input('Enter your chosen option here :-\n'))
+	
+	if d == 'one':
+		print ('\nThe answer is',(a/b))
+	
+	elif d == 'One':
+		print ('\nThe answer is',(a/b))
+		
+	elif d == 'ONE':
+		print ('\nThe answer is',(a/b))
+		
+	elif d == 'two':
+		print ('\nThe answer is',(b/a))
+	
+	elif d == 'Two':
+		print ('\nThe answer is',(b/a))
+		
+	elif d == 'TWO':
+		print ('\nThe answer is',(b/a))
+		
+	else :
+		print ('WRONG INPUT')
+		
+	
+
+
+
+
+
+
+
+
+
+else:
+	print ('OPERATION NOT FOUND')
